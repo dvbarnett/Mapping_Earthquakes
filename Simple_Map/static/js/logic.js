b9407a7ec61531b0f1c99bf8d5e41a630f28f4cb
dvbarnett <darcybarnett@gmail.com>
2022-08-29 16:58:28 -0500

    Updating branches

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -11,12 +11,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     accessToken: API_KEY
     
 });
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
