e914c345c32a69f4390ee488a8c70b03f4964b47
dvbarnett <darcybarnett@gmail.com>
2022-08-29 17:24:42 -0500

    Updating logic.js and adding cities.js

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,13 +11,19 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     maxZoom: 18,
     accessToken: API_KEY
 });
+// Get data from cities.js
+let cityData = cities;
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
-
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location,{
+        radius: city.population/100000,
+        color: "orange",
+        fillColor: "yellow"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
